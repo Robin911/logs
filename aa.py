6a549f0c14ba6cedb4ebda7741f23e22895a6a7d
Robin <robin911@foxmail.com>
2019-08-16 14:58:25 +0800

    精简代码

--- aa.py
+++ aa.py
@@ -1,10 +1,14 @@
-import re
+import datetime, re
 logline = '''123.125.71.36 - - [06/Apr/2017:18:09:25 +0800] "GET / HTTP/1.1" 200 8642 "-" "Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)"'''
-
+regex = re.compile('''(?P<remote>[\d\.]{7,}) - - \[(?P<datetime>[^\[\]]+)\] "(?P<request>[^"]+)" (?P<status>\d+) (?P<size>\d+) "(?P<aaaaaa>[^"]+)" "(?P<useragent>[^"]+)"''')
+ops = {
+    'datetime':lambda timestr:datetime.datetime.strptime(timestr, '%d/%b/%Y:%H:%M:%S %z'),
+    'status':int,
+    'size':int,
+    'request':lambda request:dict(zip(['method', 'url', 'protocol'], request.split()))
+}
 def extract(line):
-    pattern = '''(?P<remote>[\d\.]{7,}) - - \[(?P<datetime>[^\[\]]+)\] "(?P<request>[^"]+)" (?P<status>\d+) (?P<size>\d+) "([^"]+)" "(?P<useragent>[^"]+)"'''
-    regex = re.compile(pattern)
     matchar = regex.match(line)
-    return matchar.groupdict()
-
-print(extract(logline))
+    return {k:ops.get(k, lambda x:x)(v) for k, v in matchar.groupdict().items()}
+for k, v in extract(logline).items():
+    print(k, ':', v)
--- test2.py
+++ test2.py
@@ -1,28 +1,24 @@
-import datetime
-import re
+import datetime, re
 
 logline = '''123.125.71.36 - - [06/Apr/2017:18:09:25 +0800] "GET / HTTP/1.1" 200 8642 "-" "Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)"'''
-
 def extract(line):
     pattern = '''(?P<remote>[\d\.]{7,}) - - \[(?P<datetime>[^\[\]]+)\] "(?P<request>[^"]+)" (?P<status>\d+) (?P<size>\d+) "(?P<aaaaaa>[^"]+)" "(?P<useragent>[^"]+)"'''
     regex = re.compile(pattern)
     matchar = regex.match(line)
     return matchar.groupdict()
-# def convert_time(timestr):
-#     return datetime.datetime.strptime(timestr, '%d/%b/%Y:%H:%M:%S %z')
-# def convert_request(request:str):
-#     return dict(zip(['method', 'url', 'protocol'], request.split()))
-
+def convert_time(timestr):
+    return datetime.datetime.strptime(timestr, '%d/%b/%Y:%H:%M:%S %z')
+def convert_request(request:str):
+    return dict(zip(['method', 'url', 'protocol'], request.split()))
 ops = {
-    'datetime':lambda timestr:datetime.datetime.strptime(timestr, '%d/%b/%Y:%H:%M:%S %z'),
+    'datetime':convert_time,
     'status':int,
     'size':int,
-    'request':lambda request:dict(zip(['method', 'url', 'protocol'], request.split()))
+    'request':convert_request
 }
-d = {k:ops.get(k, lambda x:x)(v) for k, v in extract(logline).items()}
-# d = {}
-# for k, v in extract(logline):
-#     d[k] = ops.get(k, lambda x:x)(v)
+d = {}
+for k, v in extract(logline).items():
+    d[k] = ops.get(k, lambda x:x)(v)
 
 for k, v in d.items():
     print(k, ':', v)
