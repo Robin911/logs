35b715d254aea755893a84df4c24c210b805f3be
Robin <robin911@foxmail.com>
2019-08-23 14:51:49 +0800

    ok

--- extract2.py
+++ extract2.py
@@ -1,6 +1,7 @@
 import datetime, re
 from queue import Queue
 import threading
+from pathlib import Path
 regex = re.compile('''(?P<remote>[\d\.]{7,}) - - \[(?P<datetime>[^\[\]]+)\] "(?P<request>[^"]+)" (?P<status>\d+) (?P<size>\d+) "(?P<aaaaaa>[^"]+)" "(?P<useragent>[^"]+)"''')
 
 def extract(line):
@@ -14,7 +15,7 @@ ops = {
     'request':lambda request:dict(zip(['method', 'url', 'protocol'], request.split()))
 }
 
-def load(path:str):
+def openfile(path:str):
     with open(path) as f:
         for line in f:
             fields = extract(line)
@@ -22,6 +23,17 @@ def load(path:str):
                 yield fields
             else:
                 continue # TODO 解析失败的日志抛弃
+def load(*paths):
+    for item in paths:
+        p = Path(item)
+        if not p.exists():
+            continue
+        if p.is_dir():
+            for file in p.iterdir():
+                if file.is_file():
+                    yield from openfile(str(file))
+        elif p.is_file():
+            yield from openfile(str(p))
 
 ####################################################################################
 def window(src:Queue, handler, width: int, interval: int):
@@ -44,7 +56,13 @@ def donothing_handler(iterable:list):
     print(iterable)
     return iterable
 
-
+def status_handler(iterable):
+    status = {}
+    for item in iterable:
+        key = item['status']
+        status[key] = status.get(key, 0) + 1
+    total = len(iterable)
+    return {k:status[k]/total for k, v in status.items()}
 #################################################################################################
 # 分发器
 def dispatcher(src):
@@ -54,7 +72,6 @@ def dispatcher(src):
     def reg(handler, width:int, interval:int):
         q = Queue()
         queues.append(q)
-
         h = threading.Thread(target=window, args=(q, handler, width, interval))
         threads.append(h)
     def run():
@@ -68,7 +85,7 @@ def dispatcher(src):
 reg, run = dispatcher(load('test.log'))
 
 # 注册 窗口
-reg(donothing_handler, 10, 5)
+reg(status_handler, 7, 5)
 
 # 启动
 run()
